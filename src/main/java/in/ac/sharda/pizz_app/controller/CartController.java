18d2f0f8cf59f51dffdf8591e458b38d126b2b07
Sandeep Dogra <sandeepdogra175@gmail.com>
2019-06-08 10:08:15 +0530

    Updated

--- src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
+++ src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
@@ -1,33 +1,73 @@
 package in.ac.sharda.pizz_app.controller;
+
+
+
 import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ac.sharda.pizz_app.domain.Product;
+
 //4.) CREATED A REST CONTROLLER 
+
+
 
 @RestController
 
-public class CartController {
-	// 5.) ADDED A LIST OF CART HAVING PRODUCT
-	private List<Product> products = new ArrayList<>();
 
-	public void addProduct(Product product) {
-		this.products.add(product);
+
+public class CartController {
+
+	// 5.) ADDED A LIST OF CART HAVING PRODUCT
+
+	List<Product> del = new ArrayList<>();
+
+
+
+	public List<Product> addProduct(Product product) {
+
+		for (int i = 0; i < 20; i++) {
+
+			this.del.add(new Product(i, i * 100, "Name " + i, "Description " + i));
+
+		}
+
+		return this.del;
+
 	}
-	//*** 6.) ADDED A BEHAVIOUR MAPPING IN CARTCONTROLLER TO REMOVE ITEMS FROM THE CART
-	@GetMapping("/remove/list")
-	public void removeProduct(Product product)
-	{
-		products.remove(product);
+
+
+
+	// *** 6.) ADDED A BEHAVIOUR MAPPING IN CARTCONTROLLER TO REMOVE ITEMS FROM THE
+
+	// CART
+
+	@GetMapping("/list/delete")
+
+	public List<Product> delete(Product p) {
+
+		
+
+		return this.del;
+
 	}
+
+
 
 	@GetMapping("/count/list")
-	public int count()
-	{
-		return products.size();
+
+	public int countProduct() {
+
+		// 7.) COUNT ITEMS IN CART
+
+		return this.del.size();
+
 	}
-	
-}  
+
+
+
+}
